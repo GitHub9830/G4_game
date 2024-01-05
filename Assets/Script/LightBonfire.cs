68d24dc653cc918f90236e127b4121cf78ce1389
Hiyoko2021 <tfp422336@stu.tamagawa.ac.jp>
2024-01-05 10:22:53 +0900

    焚火の時間追加
    ・焚火の時間を増やせれるようにしました
    ・細かい調整

--- Assets/Script/LightBonfire.cs
+++ Assets/Script/LightBonfire.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class LightBonfire : MonoBehaviour
 {
     bool lightBonfire;//焚火が点いているかどうか
-    float lightTime;
+    float lightTime;//焚火の点灯時間
+    float maxLightTime;//焚火の点灯最大時間
 
     //焚火の色
     Color lightOn;//点いている
     Color lightOff;//点いていない
+
+    float addTimeScale;//焚火の追加点灯時間
 
 
     SpriteRenderer bonfireSR;//焚火のSpriteRenderer
@@ -20,6 +23,12 @@ public class LightBonfire : MonoBehaviour
         return lightBonfire;
     }
 
+    //他のスクリプトで焚火の効果時間を伸ばしたいときに呼び出してください
+    public void addLightTime()
+    {
+        if(lightTime < maxLightTime)lightTime += addTimeScale;
+    }
+
     //初期値の設定など
     void Start()
     {
@@ -27,8 +36,10 @@ public class LightBonfire : MonoBehaviour
 
         lightBonfire = false;
         lightTime = 2;
+        maxLightTime = 10;
         lightOn = new Color(1.0f, 0, 0);
         lightOff = new Color(1.0f, 1.0f, 1.0f);
+        addTimeScale = 1;
     }
 
     void Update()
@@ -47,19 +58,29 @@ public class LightBonfire : MonoBehaviour
     //焚火の時間計測
     void bonfireTimer()
     {
-        if(lightTime > 0)
+        if (lightTime > 0)
         {
-            if(!lightBonfire)lightBonfire = true;
+            //0以上の時は点灯
+            if (!lightBonfire) lightBonfire = true;
             lightTime -= Time.deltaTime;
         }
         else
         {
-            if(lightBonfire)lightBonfire = false;
+            //0以下の時は消灯
+            if (lightBonfire)
+            {
+                lightBonfire = false;
+                lightTime = 0;
+            }
         }
     }
 
     void OnTriggerStay2D(Collider2D collision)
     {
-        
+        if(collision.gameObject.tag == "Player")
+        {
+            addLightTime();//焚火の時間追加
+            Debug.Log("addTime");
+        }
     }
 }
