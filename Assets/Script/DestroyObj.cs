ecb01658966a1a4a312d0fca4efea3f5bc731588
Hiyoko2021 <tfp422336@stu.tamagawa.ac.jp>
2024-01-10 01:21:50 +0900

    現状７
    現状７

--- Assets/Script/DestroyObj.cs
+++ Assets/Script/DestroyObj.cs
@@ -14,12 +14,9 @@ public class DestroyObj: MonoBehaviour
    
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("a");
         if (collision.gameObject.CompareTag("Bonfire"))//さっきつけたTagutukeruというタグがあるオブジェクト限定で～という条件の下
         {
             Destroy(gameObject);//このゲームオブジェクトを消滅させる
         }
-
-
     }
 }
